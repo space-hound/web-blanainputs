a13a7c8ef9b0ebbb24ff21243e6dca6e5f250441
Borz ALexandru <alexandru.borz94@gmail.com>
2019-07-10 12:59:56 +0300

    total reset

--- blana-inputs/src/js/Blana/BlanaInterval/blana-interval.js
+++
@@ -1,8 +0,0 @@
-import _BlanaInterval from "./_blana-interval";
-
-export default ( element ) => {
-    let comp = new _BlanaInterval(element);
-    comp.Build();
-
-    return comp;
-}
--- blana-inputs/src/js/Blana/BlanaNumeric/blana-numeric.js
+++
@@ -1,7 +0,0 @@
-import _BlanaNumeric from "./_blana-numeric";
-
-export default ( element ) => {
-    const comp = new _BlanaNumeric(element);
-    comp.Build();
-    return comp;
-}
